a3b2ec4f9d0a1c2b12ee2e55602985580fd8d2a6
anana243 <anastasijan2005@gmail.com>
2025-10-17 05:58:39 +0100

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sandwich Shop App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -103,11 +103,10 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            children: <Widget>[
+            const Text(
+              'Welcome to My Sandwich Shop!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
           ],
         ),
